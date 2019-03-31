b8d91219b77158697ac89ae533fd68f4f4047b84
VeroMorera <47680389+VeroMorera@users.noreply.github.com>
2019-03-31 07:31:58 -0600

    Add files via upload
    Tavo, listas de utiles

---
+++ api/componentes/listaUtilesMEP/listaUtilesMEP.model.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose= require('mongoose');
+
+let schema_listaUtil= mongoose.Schema({
+    nivel : {type: String, required: true},
+    tipo_articulo : {type: String,required:true},
+    cantidad : {type: Number,required: true},
+    descripcion : {type: String, required: false}
+});
+
+module.exports=mongoose.model('ListaUtilesMEP', schema_listaUtil);
