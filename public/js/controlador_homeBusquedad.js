3029ae55828909200e6dc7491dc7adfbe018b147
VeroMorera <47680389+VeroMorera@users.noreply.github.com>
2019-04-05 22:51:15 -0600

    Add files via upload
    archivos de JS de registro de centro, home búsqueda y resultados de búsqueda. ajuste de validaciones.

--- public/js/controlador_homeBusquedad.js
+++ public/js/controlador_homeBusquedad.js
@@ -12,9 +12,29 @@ function SearchData(){
 	let filter=inputFilter.value;
 	let nivel=sltNivel.selectedOptions[0].textContent;
 
-// metodo para redireccionar a la pantalla de resultados de la busqueda se 
-// envian los datos por la url en forma de get.
+	//validar
+	let bError = false;
+
+	if (filter == '' && nivel == '' || filter == ''){
+        bError = true; 
+    	};
+
+    if(bError == true){
+        swal.fire({
+            type : 'warning',
+            buttonsStyling: false,
+            customClass: {
+            title: 'title-class',
+            confirmButton: 'confirm-button-class'},
+            text: 'Por favor ingrese un criterio en la barra de búsqueda',
+            type: 'warning',
+          });
+
+    }else{ 
+    // metodo para redireccionar a la pantalla de resultados de la busqueda se 
+	// envian los datos por la url en forma de get.
 	let b=filter+","+nivel;
-	window.location.replace("http://localhost:4000/resultado_busqueda_success.html?b="+b);
+	window.location.replace("http://localhost:4000/resultado_busqueda_success.html?b="+b);}
+
 };
 
--- public/js/controlador_registroCentroEducativo.js
+++ public/js/controlador_registroCentroEducativo.js
@@ -101,7 +101,7 @@ function obtenerDatos(){
 	//let regExpUrl = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/; //verificar url
 	let regExpEmail = /^\S+@\S+$/; //verificar email
 	let regExpPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; //contrasena 8 digitos minimo, 1 mayuscula, 1 minuscula, 1 caracter especial, 1 numero.
-	let regExpYear = /(?:(?:19|20)[0-9]{2})/; //verificar anio entre 1900 y 2099
+	let regExpYear = /(?:(?:18|20)[0-9]{2})/; //verificar anio entre 1800 y 2099
 
 	//Validar que todos los datos sean Correctos.
 	if(inputNombreComercial.value == ''){
