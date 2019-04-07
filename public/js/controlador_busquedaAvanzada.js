a775bf4a0fb118619d0592bd610f5639c06b0069
VeroMorera <47680389+VeroMorera@users.noreply.github.com>
2019-04-07 17:32:06 -0600

    Add files via upload

--- public/js/controlador_busquedaAvanzada.js
+++ public/js/controlador_busquedaAvanzada.js
@@ -1,5 +1,26 @@
 'use strict';
 
+let listaCen=obtener_lista_centros();
+let i;
+let j;
+let listaResul=[];
+
+ //____________________________________________________________________________________________________________
+/* metodo para leer los datos colectados.
+se envian atravez de la direccion mediante un get*/
+
+var url = window.location.href;
+var b = url.substring(url.indexOf("=")+1);
+let a=b.split(",");
+let filter=a[0];
+let nivel=a[1];
+let varNivel;
+let varFilter=[];
+console.log(filter);
+varFilter=a[0].split("%20");
+console.log(varFilter);
+
+
 const inputFilter= document.querySelector('#txtfil_Word');
 
 //boton 
@@ -9,10 +30,12 @@ boton.addEventListener('click' , SearchData);
 const boton2= document.querySelector('#btnNewSearch');
 boton2.addEventListener('click' , newSearch);
 
+/*
+const boton3 = document.querySelector('#btnSearchName');
+boton3.addEventListener('click' , searchByName);
+*/
 
 function SearchData(){
-	let filter=inputFilter.value;
-
 
         ShowData();
         document.getElementById("btnSearch").disabled = true;
@@ -21,33 +44,170 @@ function SearchData(){
         //
         document.getElementById("btnNewSearch").style.display = "block";
         document.getElementById("btnNewSearch").style.visibility = "visible";
-
-    };
+};
 
 function newSearch(){
     location.reload();
-
-
 };
+
+/*
+function searchByName(){
+
+    ShowDataName();
+
+    let filter=inputFilter.value;
+
+    let dato1=filter;
+    let b=dato1;
+};
+*/
 
 
 //_________________________________________________________________________________________________________________________// 
+function ShowDataName(){
 
-let listaCen=obtener_lista_centros();
+        //BUSQUEDA DE BARRA
+    // for para evaluar cada palabra ingresada en la busqueda del home
+
+            for (j=0; j < varFilter.length; j++){
+
+
+                varFilter[j]=varFilter[j].toLowerCase();
+                
+                let count=0;
+                
+                
+    // for para recorrer toda la lista de centros y evaluar palabra por palabra del for anterior
+    // y almacenar los resultados correctos en arreglo listaResul
+
+                for(i = 0; i <listaCen.length; i++){
+                 
+                  if (listaCen[i]['nombreComercial'].toLowerCase().includes(varFilter[j])){
+                    listaResul[count]=listaCen[i];
+                    count++;
+                  };
+                  if (listaCen[i]['tipoDeCentro'].toLowerCase().includes(varFilter[j])){
+                    listaResul[count]=listaCen[i];
+                    count++;
+                  };
+                  if (listaCen[i]['provincia'].toLowerCase().includes(varFilter[j])){
+                    listaResul[count]=listaCen[i];
+                    count++;
+                  };
+                  if (listaCen[i]['direccionExacta'].toLowerCase().includes(varFilter[j])){
+                    listaResul[count]=listaCen[i];
+                    count++;
+                  };
+               
+              };
+             
+    // si listaResul no tiene valores almacenados osea no hubieron coincidencias se mantiene listaCen
+    // para la evaluacion de la siguiente palabra sino se reemplaza listaCen por ListaResult
+    // reduciendo asi las posibilidades
+
+             if (listaResul.length==0){
+                listaCen=listaCen;
+             }else{
+                listaCen=listaResul;
+             };
+             
+             
+    // se setea listaResult a cero para la nueva valoracion
+
+              listaResul=[];   
+        };
+
+    //___________________________________________________________________________________________________________
+
+
+    // for para recorrer la listaCen ya depurada por el filtro
+
+        for(i = 0; i <listaCen.length; i++){
+
+    //__________________________________________________________________________________________________________
+            
+                /*metodo para la creacion de cada card para mostrar los resultados de la busqueda
+                  nota: usar un for para realizarlos de manera automatica y de una vez agregar los datos basicos
+                  la ref. los llevaria a cada centro educativo buscado.*/
+
+                 let tcards = document.querySelector('#cards');
+                  // tcards.innerHTML = '';
+                  
+                   var midiv = document.createElement("div");
+                   midiv.setAttribute("id","card"+i);
+                   midiv.setAttribute("class","card cardColor");
+                   var midiv2 = document.createElement("div");
+                   midiv2.setAttribute("class","card-body text-center");
+                   var midiv4 = document.createElement("a");
+                   midiv4.setAttribute("id","txtEnl"+i);
+                   midiv4.setAttribute("class","schoolCard");
+                   midiv4.setAttribute("href","#");
+                  
+                   var imagen2 = document.createElement('img');
+                   imagen2.setAttribute("id","imgBus"+i);
+                   imagen2.classList.add('schoolPic');
+                       if(listaCen[i]['escudo']){
+                           imagen2.src = listaCen[i]['escudo'];
+                       }else{
+                           imagen2.src = 'imgs/escudo.png';
+                       }
+                   var midiv3 = document.createElement("p");
+                   midiv3.setAttribute("id","txtNameCen"+i);
+                   midiv3.setAttribute("class","card-text");
+                   midiv3.innerHTML = listaCen[i]['nombreComercial'];
+                   document.querySelector("#cards").appendChild(midiv);
+                   document.querySelector("#card"+i).appendChild(midiv2);
+                   document.querySelector("#card"+i).appendChild(midiv4);
+                   document.querySelector("#txtEnl"+i).appendChild(imagen2);
+                   document.querySelector("#card"+i).appendChild(midiv3);
+       
+    //________________________________________________________________________________________________________
+              //tabla oculta con resultados     
+            
+                   let tbody = document.querySelector('#tblCentros tbody');
+                   tbody.innerHTML = '';
+                   let fila = tbody.insertRow();
+       
+                
+       
+                   let celdaNombreComercial = fila.insertCell();
+                   let celdaTipodeCentro = fila.insertCell();
+                   let celdaTelefonoCtro = fila.insertCell();
+                   let celdaFax = fila.insertCell();
+                   let celdaSitioWeb = fila.insertCell();
+                   let celdaFacebook = fila.insertCell();
+                   let celdaEmailInstit = fila.insertCell();
+                   let celdaAnoFund = fila.insertCell();
+                   let celdaDireccionExacta= fila.insertCell();
+                   let celdaEscudo = fila.insertCell();
+                   
+                   celdaNombreComercial.innerHTML = listaCen[i]['nombreComercial'];
+                   celdaTipodeCentro.innerHTML = listaCen[i]['tipoDeCentro'];
+                   celdaTelefonoCtro.innerHTML = listaCen[i]['telefonoCtro'];
+                   celdaFax.innerHTML = listaCen[i]['fax'];
+                   celdaSitioWeb.innerHTML = listaCen[i]['sitioWeb'];
+                   celdaFacebook.innerHTML = listaCen[i]['facebook'];
+                   celdaEmailInstit.innerHTML = listaCen[i]['emailInstit'];
+                   celdaAnoFund.innerHTML = listaCen[i]['anoFund'];
+                   celdaDireccionExacta.innerHTML = listaCen[i]['direccionExacta'];
+                   
+       
+                   let imagen = document.createElement('img');
+                       imagen.classList.add('imagenTabla'); 
+       
+                       if(listaCen[i]['escudo']){
+                           imagen.src = listaCen[i]['escudo'];
+                       }else{
+                           imagen.src = 'imgs/escudo.png';
+                       }
+       
+                       celdaEscudo.appendChild(imagen);  
+                    };
+
+    };
+ 
 
 function ShowData(){
-
-
-        //____________________________________________________________________________________________________________
-        /* metodo para leer los datos colectados en el formulario de home.html
-        se envian atravez de la direccion mediante un get*/
-
-        var url = window.location.href;
-        var b = url.substring(url.indexOf("=")+1);
-        let a=b.split(",");
-        let filter=a[0];
-        let nivel=a[1];
-
 
         for(let i = 0; i < listaCen.length; i++){
 
@@ -288,15 +448,15 @@ function ShowData(){
                     (listaCen[i]['secundaria'] == true && document.getElementById("cbSecundaria").checked &&
                      listaCen[i]['tipoDeCentro'] == 'Privado' && document.getElementById("cbPrivado").checked &&  
                      listaCen[i]['religioso'] == true && document.getElementById("cbReligioso").checked) ||
-                    //si es secundaria publica religios
+                    //si es secundaria publica religioso
                     (listaCen[i]['secundaria'] == true && document.getElementById("cbSecundaria").checked &&
                      listaCen[i]['tipoDeCentro'] == 'Publico' && document.getElementById("cbPublico").checked &&  
                      listaCen[i]['religioso'] == true && document.getElementById("cbReligioso").checked) ||
-                    // si es primaria privada religios
+                    // si es primaria privada religioso
                     (listaCen[i]['primaria'] == true && document.getElementById("cbPrimaria").checked &&
                      listaCen[i]['tipoDeCentro'] == 'Privado' && document.getElementById("cbPrivado").checked && 
                      listaCen[i]['religioso'] == true && document.getElementById("cbReligioso").checked) || 
-                    // si es primaria publica religios
+                    // si es primaria publica religioso
                     (listaCen[i]['primaria'] == true && document.getElementById("cbPrimaria").checked &&
                      listaCen[i]['tipoDeCentro'] == 'Publico' && document.getElementById("cbPublico").checked && 
                      listaCen[i]['religioso'] == true && document.getElementById("cbReligioso").checked) ){
