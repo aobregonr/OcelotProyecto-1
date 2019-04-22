8772e01dc984d457f14df4706758f7fc27e96f51
adriancr93 <aobregonr@ucenfotec.ac.cr>
2019-04-21 20:53:47 -0600

    Update agendarCita.api.js

---
+++ api/component/agendarCita/agendarCita.api.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const agendModel = require('./agendarCita.model');
+
+module.exports.registrar = function(req, res){
+
+    let nuevaCita = new agendModel({
+        fecha: req.body.fecha,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email,
+        hora: req.body.hora,
+        telefono: req.body.telefono,
+        descripcion: req.body.descripcion
+
+    });
+
+    nuevaCita.save(function(error){
+         if (error) {
+             res.json({
+                 success: false,
+                 msg: 'No se pudo agendar la cita, ocurrió el siguiente error ' + error
+             });
+         } else {
+             res.json({
+                 success: true,
+                 msg: 'Se pudo agendar la cita con exito'
+             });
+         }
+    })
+
+};
+
