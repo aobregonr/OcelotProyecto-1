a29c6a6269c6a179dc365ef48df93e6ae377d47d
VeroMorera <47680389+VeroMorera@users.noreply.github.com>
2019-04-25 10:42:29 -0600

    con header y footer

--- public/js/controlador_nav.js
+++ public/js/controlador_nav.js
@@ -2,26 +2,65 @@
 
 let conectado = sessionStorage.getItem('conectado');
 let tipoUsuario = sessionStorage.getItem('tipo_usuario');
+let nombreUsuario = sessionStorage.getItem('nombre_usuario');
+let apellidoUsuario = sessionStorage.getItem('apellido_usuario');
 
-if(conectado){
-    switch(tipoUsuario){
+
+if (conectado) {
+    document.querySelector('#invitado').classList.add('hide'); //esconder user invitado
+    document.querySelector('#logeado').classList.remove('hide'); //mostrar user logeado 
+
+
+     switch(tipoUsuario){
         case 'admin':
 
+        //poner el nombre del admin en el header
+        document.querySelector('#logeado').innerHTML = '<a class="headerlink" href="perfilAdmin.html"><i class="fas fa-user"></i>'+' ' + nombreUsuario + ' '+ apellidoUsuario + ' (Admin)' + '</a>'; //perfil admin
+
         break;
+
         case 'CentroEducativo':
-         
-        
+
+        //poner el nombre del centro en el header
+        document.querySelector('#logeado').innerHTML = '<a class="headerlink" href="perfilCentroEducEdit.html"><i class="fas fa-user"></i>'+' ' + nombreUsuario + '</a>'; //perfil del centro
+
+        //restringir
+        navBusqueda.classList.add('hide');  //ocultar busqueda avanzada
+        navCentros.classList.add('hide');  //ocultar todos los colegios
+        linkCentros.classList.add('hide'); 
+        linkCentrosLi.classList.add('hide'); 
+        olvidoContrasenna.classList.add('hide'); 
+        advanceSearch.href = '#';
+
         break;
+
         case 'PadreFam':
+
+        //poner el nombre del padre de fam en el header
+        document.querySelector('#logeado').innerHTML = '<a class="headerlink" href="perfilUsuario.html"><i class="fas fa-user"></i>'+' ' + nombreUsuario + ' '+ apellidoUsuario + '</a>'; //perfil padre fam
+
+        //restringir
+        olvidoContrasenna.classList.add('hide'); 
 
         break;
     }
+
 } else {
-
-   window.location.href = 'index.html';   
-    $("#iniSesion").modal()
-
+    document.querySelector('#cerrarsesion').classList.add('hide'); //esconder cerrar sesion mientras user invitado este activo.
 }
+
+
+const botonCerrarSesion = document.querySelector('#cerrarsesion');
+
+
+function cerrarSesion(){
+    sessionStorage.clear();
+    window.location.href = 'index.html';
+}
+
+botonCerrarSesion.addEventListener('click', cerrarSesion);
+
+
 
 
 function accesoRestringido(){
