c6b962bacaa5944e600513c46442b12d3c803fe2
GJMachado <gjimenezm@ucenfotec.ac.cr>
2019-04-25 23:40:14 -0600

    Create servicio_comentarios.js

---
+++ public/js/servicio_comentarios.js
@@ -0,0 +1,56 @@
+'use strict';
+
+
+
+
+
+function registrar_comentario( pcod, ppadre, pfoto, pcomentario){
+    let request= $.ajax({
+        url: 'http://localhost:4000/api/registrar_comment',
+        method: "POST",
+        data: {
+            cod: pcod,
+            padre: ppadre,
+            foto: pfoto,
+            comentario : pcomentario
+        },
+        dataType: "json",
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
+    });
+
+    request.done(function(res){
+       
+    });
+
+    request.fail(function(res){
+        swal.fire({
+            type: 'error',
+            title: 'Proceso inconcluso',
+            text: res.msg
+        });
+    });
+};
+
+
+function obtener_comentarios(){
+    let listaComment = [];
+    let peticion = $.ajax({
+        url: 'http://localhost:4000/api/listar_comment',
+        type: 'get',
+        contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType : 'json',
+        async:false,
+        data:{
+        }
+      });
+    
+      peticion.done(function(comment){
+        listaComment = comment;
+      });
+    
+      peticion.fail(function(){
+       
+      });
+      
+    return listaComment;
+};
