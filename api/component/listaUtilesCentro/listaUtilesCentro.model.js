201ed4b1ce70e892f77d93e2bc7fbd4e2c459ea9
GJMachado <gjimenezm@ucenfotec.ac.cr>
2019-05-03 00:11:20 -0600

    Update listaUtilesCentro.model.js

--- api/component/listaUtilesCentro/listaUtilesCentro.model.js
+++ api/component/listaUtilesCentro/listaUtilesCentro.model.js
@@ -3,6 +3,8 @@
 const mongoose= require('mongoose');
 
 let schema_listaUtil= mongoose.Schema({
+    cod : {type: String, required: true},
+    centroName : {type: String, required: true},
     nivel : {type: String, required: true},
     tipo_articulo : {type: String,required:true},
     cantidad : {type: Number,required: true},
